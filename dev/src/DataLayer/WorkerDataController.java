56220f1c727202f8605c719afcacf9fe4a428941
Dan Roizman <danroiz@post.bgu.ac.il>
2021-04-27 19:03:42 +0300

    adde jdbc library

--- dev/src/DataLayer/WorkerDataController.java
+++ dev/src/DataLayer/WorkerDataController.java
@@ -3,9 +3,29 @@ import java.sql.*;
 
 public class WorkerDataController {
     private IdentityMap identityMap;
-
+    private Connection conn;
 
     public WorkerDataController(){
         this.identityMap = new IdentityMap();
+        conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:WorkersDB.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
--- dev/src/main.java
+++ dev/src/main.java
@@ -1,8 +1,10 @@
+import DataLayer.WorkerDataController;
 import PresentationLayer.Main_Menu;
 
 public class main {
     public static void main(String[] args){
-        Main_Menu menu = new Main_Menu();
-        menu.start();
+        //Main_Menu menu = new Main_Menu();
+        //menu.start();
+        WorkerDataController workerDataController = new WorkerDataController();
     }
 }
