c9412176da7de48ced9b9e9a2b6f1dd9168c2984
guy <guyporat98@gmail.com>
2021-05-05 12:08:25 +0300

    Revert " tttthd"
    This reverts commit e5d6764615deb1230816fde844d0e352f3d51635.

---
+++ dev/src/BusinessLayer/Transport_BusinessLayer/Shops/Supplier.java
@@ -0,0 +1,45 @@
+package BusinessLayer.Transport_BusinessLayer.Shops;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Supplier extends Site {
+
+
+    private List<Product> productsServed;
+
+    //basic constructor
+    public Supplier(String name, int id, String phoneNumber, String contact, Area storeArea) {
+        super(name, id, phoneNumber, contact, storeArea);
+        productsServed = new LinkedList<>();
+    }
+
+    //constructor for full reload
+    public Supplier(String name, int id, String phoneNumber, String contact, Area storeArea, List<Product> productsServed) {
+        super(name, id, phoneNumber, contact, storeArea);
+        this.productsServed = productsServed;
+    }
+    public String toString() {
+        return "Supplier ID: " + id + ", " + name + ", " + StoreArea + ", " + contact + ", " + phoneNumber + "\n";
+    }
+
+
+    public List<Product> getProductsServed() {
+        return productsServed;
+    }
+
+    public void setProductsServed(List<Product> productsServed) {
+        this.productsServed = productsServed;
+    }
+
+    public void addProduct(Product prod)
+    {
+        productsServed.add(prod);
+    }
+    public void removeProduct(Product prod)
+    {
+        productsServed.remove(prod);
+    }
+
+
+}
---
+++ dev/src/main.java
@@ -0,0 +1,9 @@
+import BusinessLayer.Workers_BusinessLayer.Workers.Job;
+import PresentationLayer.Main_Menu;
+
+public class main {
+    public static void main(String[] args){
+        Main_Menu menu = new Main_Menu();
+        menu.start();
+    }
+}
